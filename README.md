fe473d15a8e1635998403a1bb2ba5fab5f7bbcb8
mlb <mlb>
2017-03-06 01:41:41 +0000

    basic functionality implemented

---
+++ README.md
@@ -0,0 +1,5 @@
+To use, copy all the text in invoice pdf file (ctrl+a, ctrl+c) into a .txt file, then run
+
+invoiceReader.py example.txt
+
+and it will print out your hourly pay.
---
+++ invoiceReader.py
@@ -0,0 +1,18 @@
+import sys, re
+
+def time_difference(time1, time2): #returns difference in hours between two times in the form hh:mm
+	return int(float(time2.split(":")[0])) - int(float(time1.split(":")[0])) + (int(float(time2.split(":")[1])) - int(float(time1.split(":")[1])))/60
+
+file = open(str(sys.argv[1])) 
+file_text = file.read()
+times = re.findall(r'\d\d:\d\d \d\d:\d\d', file_text)
+total_time = 0
+print(file_text)
+for time_pair in times:
+	total_time += time_difference(time_pair.split(" ")[0], time_pair.split(" ")[1])
+total_pay = float(file_text.split("\n")[-2].split('£')[1]);
+print(total_pay/total_time)
+# total_income_line = re.search(r'Total £(\d)*.\d\d', file_text)
+# total_income = float(total_income_line.split('£')[1].
+# print("£" + str(total_income/total_time))
+
